121b0f7af93085498efff7683b99998987527f1a
PERRET Jeremie <jeremie.perret@sword-group.com>
2024-07-29 16:48:11 +0200

    naming conventions and adapting font-size for full HD

--- templates/template-home.php
+++ templates/template-home.php
@@ -10,19 +10,27 @@
 <?php get_header(); ?>
 
 <main>
-    <div id="main-title">
+    <div id="main_title">
         <h1>BIENVENUE CHEZ</h1>
-        <div id="main-sub-title">
+        <div id="main_sub_title">
             <img id="processor" src="<?php echo THEME_IMG_PATH; ?>/processor.png" />
             <h1>PC-BOUTIQUE</h1>
         </div>
     </div>
     <div id="banners">
-        <div class="banner" id="main-left-banner">
-            <p>Découvrez nos offres principales en matière de produits informatiques et de services de montage, nettoyage et réparation de PC.<br />
+        <div class="banner" id="main_left_banner">
+            <p class="inter">Découvrez nos offres principales en matière de produits informatiques et de services de montage, nettoyage et réparation de PC.<br />
             Nous sommes spécialisés dans la fourniture de solutions informatiques adaptées à vos besoins, que vous soyez un gamer passionné ou un professionnel en quête de performance ou de qualité.</p>
         </div>
-        <div class="banner"></div>
+        <div class="banner" id="c"></div>
+        <div id="visit_shop_group">
+            <div id="visit_group_headline">
+                <span id="shop_rounded"><img id="icon_shop" src="<?php echo THEME_IMG_PATH; ?>/white_shop.svg" alt=""></span>
+                <p>VISITEZ NOTRE<br />
+                    BOUTIQUE</p>
+            </div>
+            <span></span>
+        </div>
     </div>
 </main>
 
