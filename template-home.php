9f4e545d61a0c29646911151458d52d61f0dd35f
PERRET Jeremie <jeremie.perret@sword-group.com>
2024-07-29 15:09:02 +0200

    first try for services block

--- template-home.php
+++ template-home.php
@@ -9,6 +9,21 @@
 
 <?php get_header(); ?>
 
-<?php the_content() ?>
+<main>
+    <div id="main-title">
+        <h1>BIENVENUE CHEZ</h1>
+        <div id="main-sub-title">
+            <img id="processor" src="<?php echo THEME_IMG_PATH; ?>/processor.png" />
+            <h1>PC-BOUTIQUE</h1>
+        </div>
+    </div>
+    <div id="banners">
+        <div class="banner" id="main-left-banner">
+            <p>Découvrez nos offres principales en matière de produits informatiques et de services de montage, nettoyage et réparation de PC.<br />
+            Nous sommes spécialisés dans la fourniture de solutions informatiques adaptées à vos besoins, que vous soyez un gamer passionné ou un professionnel en quête de performance ou de qualité.</p>
+        </div>
+        <div class="banner"></div>
+    </div>
+</main>
 
 <?php get_footer(); ?>
--- template-shop.php
+++ template-shop.php
@@ -5,3 +5,10 @@
  *
  * @package pc_boutique
  */
+?>
+
+<?php get_header(); ?>
+
+<?php the_content() ?>
+
+<?php get_footer(); ?>
