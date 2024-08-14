1cd710ef67d814fcc827d1ad8e5ff11290f1dc23
PERRET Jeremie <jeremie.perret@sword-group.com>
2024-08-14 22:18:08 +0200

    add container contact

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -9,21 +9,36 @@
 
 <?php get_header(); ?>
 
-<article id="contact_container">
-	<h1>CONTACTEZ</h1>
-	<div class="contact_info">
-		<span id="span_call"><img id="call_icon" src="<?php echo THEME_IMG_PATH; ?>/call.svg" alt=""></span>
-		<p>+41 78 934 19 56</p>
-	</div>
-	<div class="contact_info">
-		<span><img id="location_icon" src="<?php echo THEME_IMG_PATH; ?>/location_on.svg" alt=""></span>
-		<p>Route de Malcroissant 18,<br>1295 Tannay</p>
-	</div>
-	<div class="contact_info">
-		<span><img id="call_icon" src="<?php echo THEME_IMG_PATH; ?>/schedule.svg" alt=""></span>
-		<p>Lundi-Vendredi : 17h30-22h00<br>Samedi: sur rendez-vous</p>
-	</div>
-</article>
+<main id="main_contact">
+	<div id="left">
+		<article id="contact_container">
+			<h1>CONTACTEZ</h1>
+			<div class="contact_info">
+				<span id="span_call"><img id="call_icon" src="<?php echo THEME_IMG_PATH; ?>/call.svg" alt=""></span>
+				<p>+41 78 934 19 56</p>
+			</div>
+			<div class="contact_info">
+				<span><img id="location_icon" src="<?php echo THEME_IMG_PATH; ?>/location_on.svg" alt=""></span>
+				<p>Route de Malcroissant 18,<br>1295 Tannay</p>
+			</div>
+			<div class="contact_info">
+				<span><img id="call_icon" src="<?php echo THEME_IMG_PATH; ?>/schedule.svg" alt=""></span>
+				<p>Lundi-Vendredi : 17h30-22h00<br>Samedi: sur rendez-vous</p>
+			</div>
+		</article>
 
-<img id="map" src="<?php echo THEME_IMG_PATH; ?>/map.png" alt="">
+		<a href="https://maps.app.goo.gl/VUWe1EXKELCp89XU7" target="_blank" rel="noopener noreferrer"><img id="map" src="<?php echo THEME_IMG_PATH; ?>/map.png" alt=""></a>
+	</div>
+	<article id="contact_form_container">
+		<p class="inter">Nous sommes à votre disposition pour répondre à toutes vos questions et demandes. Remplissez le formulaire ci-dessous ou utilisez les coordonnées fournies pour nous contacter directement.</p>
+		<div id="contact_form">
+			<input type="text" name="name" id="f_name" placeholder="nom*">
+			<input type="text" name="email" id="f_email" placeholder="email*">
+			<input type="text" name="phone" id="f_phone" placeholder="téléphone*">
+			<input type="text" name="message" id="f_messgae" placeholder="message*">
+		</div>
+	</article>
+</main>
+
+
 <?php get_footer(); ?>
